089cc637f780afd39e1c252122caa4d3c797d2ed
L1ndseyHerman <lherman21@jcu.edu>
2021-10-17 21:36:08 -0400

    Kind of a phone view

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 
+import classes from "./Nav.module.css";
+
 import NavLink from "./NavLink";
 
 const Nav: React.FC = () => {
   return (
-    <div>
-      <NavLink />
-      <NavLink />
-      <NavLink />
-      <NavLink />
-      <NavLink />
-      <NavLink />
+    <div className={classes.nav}>
+      <NavLink text="Home" />
+      <NavLink text="Education" />
+      <NavLink text="Jobs" />
+      <NavLink text="Skills" />
+      <NavLink text="Hobbies" />
+      <NavLink text="Contact" />
+      <NavLink text="JS Skills" />
+      <NavLink text="Fake Webpages" />
     </div>
   );
 };
--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import classes from "./NavLink.module.css";
 
-const NavLink: React.FC = () => {
-  return <p className={classes.navLink}>Not a real link yet.</p>;
+const NavLink: React.FC<{ text: string }> = (props) => {
+  return <p className={classes.navLink}>{props.text}</p>;
 };
 
 export default NavLink;
