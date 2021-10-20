d0ba391a86dd4a0162c4e7fa555b336f394c620a
L1ndseyHerman <lherman21@jcu.edu>
2021-10-20 11:30:45 -0400

    I think this is a better way to linebreak and center the Jobs radio buttons than just <br />?

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import Wrapper from "../components/Wrapper";
+import classes from "./Jobs.module.css";
 
 const Jobs: React.FC = () => {
   return (
     <div>
       <Wrapper title="Jobs">
-        <p>Jobs Text</p>
+        <fieldset className={classes.fieldset}>
+          <form className={classes.form}>
+            <h4 className={classes.h4}>Select a Job to Learn More About it:</h4>
+            <label htmlFor="def">
+              <input
+                id="def"
+                type="radio"
+                name="group"
+                value="definitely"
+                checked
+              />
+              Boyd Watterson Asset Manangement
+            </label>
+            <label htmlFor="maybe">
+              <input id="maybe" type="radio" name="group" value="maybe" />
+              Maybe
+            </label>
+            <label htmlFor="no">
+              <input id="no" type="radio" name="group" value="not-sure" />
+              Not Sure
+            </label>
+          </form>
+        </fieldset>
       </Wrapper>
     </div>
   );
