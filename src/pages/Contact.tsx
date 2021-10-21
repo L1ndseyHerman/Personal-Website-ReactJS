55fe185dbe6f3c6ddaa4cac06cda143959320549
L1ndseyHerman <lherman21@jcu.edu>
2021-10-21 15:47:59 -0400

    Imported an image, started the CSS for animating it.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Wrapper from "../components/Wrapper";
 import classes from "./Contact.module.css";
+import bobImage from "./bobseventy.png";
 
 const Contact: React.FC = () => {
   return (
     <div>
       <Wrapper title="Contact">
+        <img className={classes.bob} src={bobImage} alt="Moving Bob!" />
         <p>
           Email:&nbsp;
           <a href="mailto:lindseyherman95@yahoo.com" className={classes.link}>
