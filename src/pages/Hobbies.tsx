235966241cc65144cf4fae4068028fcbfbf97da1
L1ndseyHerman <lherman21@jcu.edu>
2021-10-21 19:32:44 -0400

    Got the selected hobby value to display.

--- src/pages/Hobbies.tsx
+++ src/pages/Hobbies.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Wrapper from "../components/Wrapper";
 import classes from "./Hobbies.module.css";
 
 const Hobbies: React.FC = () => {
+  const [hobby, setHobby] = useState("");
+
+  const selectChangeHandler: (
+    event: React.ChangeEvent<{ value: string }>
+  ) => void = (event) => {
+    setHobby(event.target.value);
+  };
+
   return (
     <div>
       <Wrapper title="Hobbies">
-        <select id="hobbyChoices" className={classes.select}>
-          <option>Hobbies:</option>
+        <select
+          id="hobbyChoices"
+          className={classes.select}
+          onChange={(event: React.ChangeEvent<{ value: string }>) =>
+            selectChangeHandler(event)
+          }
+        >
+          <option value="nothing">Hobbies:</option>
           <option value="music">Music</option>
           <option value="creativeWriting">Creative Writing</option>
           <option value="nature">Nature</option>
@@ -15,10 +29,7 @@ const Hobbies: React.FC = () => {
         </select>
         <div className={classes.imageDiv}>
           <div className={classes.infoDiv}>
-            <p>
-              Some text aepwoirppaewj rtpriuwtriojgfniu joareojtnlaiurgj
-              ofksjgnalriegj
-            </p>
+            <p>Some text Hobby: {hobby}</p>
           </div>
         </div>
       </Wrapper>
