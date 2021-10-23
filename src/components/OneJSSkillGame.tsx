12e13186ca31be2162688ca91dee165011b846b5
L1ndseyHerman <lherman21@jcu.edu>
2021-10-23 12:49:24 -0400

    CSS for the OneJSSkillGame component is done, now to make the games.

---
+++ src/components/OneJSSkillGame.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./OneJSSkillGame.module.css";
+
+const OneJSSkillGame: React.FC = () => {
+  return (
+    <div className={classes.blueDiv}>
+      <div className={classes.infoDiv}>
+        <p>One Game</p>
+      </div>
+    </div>
+  );
+};
+
+export default OneJSSkillGame;
--- src/pages/JS_Skills.tsx
+++ src/pages/JS_Skills.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Wrapper from "../components/Wrapper";
+import OneJSSkillGame from "../components/OneJSSkillGame";
 
 const JS_Skills: React.FC = () => {
   return (
     <div>
       <Wrapper title="JS Skills">
-        <p>JS Skills Text</p>
+        <OneJSSkillGame />
+        <OneJSSkillGame />
+        <OneJSSkillGame />
+        <OneJSSkillGame />
       </Wrapper>
     </div>
   );
