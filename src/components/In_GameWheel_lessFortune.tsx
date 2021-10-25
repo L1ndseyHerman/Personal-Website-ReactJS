0fdbc812aab7fe998ed1fda1f32e26e37885c0eb
L1ndseyHerman <lherman21@jcu.edu>
2021-10-25 12:38:58 -0400

    Wheel-less Fortune only accepts one letter guess at a time.

--- src/components/In_GameWheel_lessFortune.tsx
+++ src/components/In_GameWheel_lessFortune.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import classes from "./In_GameWheel_lessFortune.module.css";
 
 type ThisTurnSettings = { wonThisTurn: boolean; lostThisTurn: boolean };
@@ -6,17 +6,37 @@ type ThisTurnSettings = { wonThisTurn: boolean; lostThisTurn: boolean };
 const In_GameWheel_lessFortune: React.FC<{
   clickHandler: ({ wonThisTurn, lostThisTurn }: ThisTurnSettings) => void;
 }> = (props) => {
+  //  Hard-coding the easy amounts for now:
+  const [whatYouSolvedSoFar, setWhatYouSolvedSoFar] = useState("_ _ _ ");
+  const [guessesLeft, setGuessesLeft] = useState(12);
+  const [inputHasError, setInputHasError] = useState(false);
+  const theAnswer = "cat";
+  const guessRef = useRef<HTMLInputElement>(null);
+
+  //	Makes sure the input is just one lower-case letter.
+  const checkInputForErrors = () => {
+    const inputString = guessRef.current!.value;
+    //	Makes sure the letter is both the start and the end of the regex, meaning nothing else can be there.
+    const regex = /^[a-z]$/;
+    const hasOnlyOneLower = inputString.match(regex);
+    if (hasOnlyOneLower) {
+      setInputHasError(false);
+      //checkIfLetterInWord(inputString);
+      props.clickHandler({ wonThisTurn: true, lostThisTurn: false });
+    } else {
+      setInputHasError(true);
+    }
+  };
+
   return (
     <>
-      <button
-        className={classes.button}
-        onClick={() =>
-          props.clickHandler({ wonThisTurn: true, lostThisTurn: false })
-        }
-      >
-        A Button
+      <input type="text" ref={guessRef} />
+      <button className={classes.button} onClick={checkInputForErrors}>
+        Guess A Letter
       </button>
-      <p>Some blanks</p>
+      {inputHasError && <p>Please guess one lower-case letter at a time.</p>}
+      <p>{whatYouSolvedSoFar}</p>
+      <p>Guesses left: {guessesLeft}</p>
     </>
   );
 };
