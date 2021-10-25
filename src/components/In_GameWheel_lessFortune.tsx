f91cd3f714d0e2bae8a92d0eeb94511bfbee332f
L1ndseyHerman <lherman21@jcu.edu>
2021-10-25 16:55:28 -0400

    solveEntirePuzzle() works

--- src/components/In_GameWheel_lessFortune.tsx
+++ src/components/In_GameWheel_lessFortune.tsx
@@ -12,6 +12,7 @@ const In_GameWheel_lessFortune: React.FC<{
   const [inputHasError, setInputHasError] = useState(false);
   const theAnswer = "cat";
   const guessRef = useRef<HTMLInputElement>(null);
+  const solveRef = useRef<HTMLInputElement>(null);
 
   const checkInputForOneLowercaseLetter = () => {
     const inputString = guessRef.current!.value;
@@ -61,6 +62,31 @@ const In_GameWheel_lessFortune: React.FC<{
     guessRef.current!.value = "";
   };
 
+  const solveEntirePuzzle = () => {
+    const wordGuess = solveRef.current!.value;
+    let won = true;
+
+    for (let index = 0; index < theAnswer.length; index++) {
+      if (index >= wordGuess.length || theAnswer[index] !== wordGuess[index]) {
+        won = false;
+      }
+    }
+    if (won) {
+      props.clickHandler({ wonThisTurn: true, lostThisTurn: false });
+    } else {
+      setGuessesLeft((prevNumberOfGuesses) => {
+        //  This if-check needs to be here bec will be 1 behind in a seperate function
+        //  due to batch updates!
+        if (prevNumberOfGuesses === 1) {
+          props.clickHandler({ wonThisTurn: false, lostThisTurn: true });
+        }
+        return prevNumberOfGuesses - 1;
+      });
+
+      solveRef.current!.value = "";
+    }
+  };
+
   return (
     <>
       <input type="text" ref={guessRef} className={classes.input} />
@@ -70,8 +96,10 @@ const In_GameWheel_lessFortune: React.FC<{
       >
         Guess A Letter
       </button>
-      <button className={classes.buttonRight}>Solve the Puzzle</button>
-      <input type="text" className={classes.inputRight} />
+      <button className={classes.buttonRight} onClick={solveEntirePuzzle}>
+        Solve the Puzzle
+      </button>
+      <input type="text" ref={solveRef} className={classes.inputRight} />
       {inputHasError && (
         <p className={classes.par}>
           Please guess one lower-case letter at a time.
